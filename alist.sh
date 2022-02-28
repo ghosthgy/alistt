c27e7181f22dbe3a8e4cdb308adabd2c172575ab
kamileecher2 <91373907+kamileecher2@users.noreply.github.com>
2022-02-28 15:54:01 +0300

    Add files via upload

---
+++ alist.sh
@@ -0,0 +1,33 @@
+# 创建配置文件目录
+mkdir -p /alist/config/temp
+
+cat >/alist/config/config.json <<EOF
+{
+  "address": "0.0.0.0",
+  "port": $PORT,
+  "assets": "/",
+  "database": {
+    "type": "$ADATABASE",
+    "user": "$BSQLUSER",
+    "password": "$CSQLPASSWORD",
+    "host": "$DSQLHOST",
+    "port": $ESQLPORT,
+    "name": "$FSQLNAME",
+    "table_prefix": "x_",
+    "db_file": "/alist/config/data.db"
+  },
+  "scheme": {
+    "https": false,
+    "cert_file": "",
+    "key_file": ""
+  },
+  "cache": {
+    "expiration": $GEXPIRATION,
+    "cleanup_interval": $HCLEANUP_INTERVAL
+  },
+  "temp_dir": "/alist/config/temp"
+}
+EOF
+
+cd /alist
+./alist -conf config/config.json
